a019e8ccdbbac5547947d7ee5aee715a0faf711b
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-02-11 20:57:05 +0000

    working on AddModule

---
+++ app/src/main/java/com/landtanin/studentattendancecheck/fragment/AddModuleFragment.java
@@ -0,0 +1,69 @@
+package com.landtanin.studentattendancecheck.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.landtanin.studentattendancecheck.R;
+
+
+/**
+ * Created by nuuneoi on 11/16/2014.
+ */
+public class AddModuleFragment extends Fragment {
+
+    public AddModuleFragment() {
+        super();
+    }
+
+    public static AddModuleFragment newInstance() {
+        AddModuleFragment fragment = new AddModuleFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_add_module, container, false);
+        initInstances(rootView);
+        return rootView;
+    }
+
+    private void initInstances(View rootView) {
+        // Init 'View' instance(s) with rootView.findViewById here
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    /*
+     * Save Instance State Here
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save Instance State here
+    }
+
+    /*
+     * Restore Instance State Here
+     */
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null) {
+            // Restore Instance State here
+        }
+    }
+}
