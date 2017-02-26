e898e7cc334073bf175bfbe0f9121af0a7c8d82e
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-02-26 19:36:11 +0000

    add ModuleItemList List in AddModuleAdapter

--- app/src/main/java/com/landtanin/studentattendancecheck/adapter/AddModuleAdapter.java
+++ app/src/main/java/com/landtanin/studentattendancecheck/adapter/AddModuleAdapter.java
@@ -1,5 +1,6 @@
 package com.landtanin.studentattendancecheck.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,11 +10,22 @@ import android.widget.TextView;
 
 import com.landtanin.studentattendancecheck.R;
 
+import java.util.List;
+
 /**
  * Created by landtanin on 2/15/2017 AD.
  */
 
 public class AddModuleAdapter extends RecyclerView.Adapter<AddModuleAdapter.RecyclerViewHolder> {
+
+    private List<AddModuleItem> mModuleItemList;
+    Context mContext;
+
+    public AddModuleAdapter(List<AddModuleItem> moduleItemList, Context context) {
+        mModuleItemList = moduleItemList;
+        mContext = context;
+    }
+
     @Override
     public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_add_module, parent, false);
@@ -23,8 +35,10 @@ public class AddModuleAdapter extends RecyclerView.Adapter<AddModuleAdapter.Recy
     @Override
     public void onBindViewHolder(RecyclerViewHolder holder, int position) {
 
-        holder.addModuleModuleNameTxt.setText("Module" + position);
-        holder.addModuleModuleIdTxt.setText("00000" + position);
+        AddModuleItem addModuleItem = mModuleItemList.get(position);
+
+        holder.addModuleModuleNameTxt.setText(addModuleItem.getModuleText());
+        holder.addModuleModuleIdTxt.setText(addModuleItem.getModuleId());
         holder.addModuleCheckBox.setChecked(false);
 
     }
