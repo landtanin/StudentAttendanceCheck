436521c3d674ff31216304047cde465cc0021b46
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-03-05 16:19:00 +0000

    done button back to fragmentHome

--- app/src/main/java/com/landtanin/studentattendancecheck/activity/MainActivity.java
+++ app/src/main/java/com/landtanin/studentattendancecheck/activity/MainActivity.java
@@ -6,11 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.landtanin.studentattendancecheck.R;
 import com.landtanin.studentattendancecheck.databinding.ActivityMainBinding;
+import com.landtanin.studentattendancecheck.fragment.FragmentHome;
 import com.landtanin.studentattendancecheck.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding b;
+    private boolean fakeBol = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,14 @@ public class MainActivity extends AppCompatActivity {
                             "MainFragment")
                     .commit();
 
+            FragmentHome fragmentHome = FragmentHome.newInstance();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentContainer,
+                            fragmentHome,
+                            "FragmentHome")
+                    .detach(fragmentHome)
+                    .commit();
+
         }
 
     }
@@ -37,4 +47,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+
+        // TODO: check if there's data in the database, delete fakeBol
+        if (fakeBol) {
+            fakeBol = false;
+            super.onResume();
+        } else {
+
+            MainFragment mainFragment = (MainFragment)
+                    getSupportFragmentManager().findFragmentByTag("MainFragment");
+            FragmentHome fragmentHome = (FragmentHome)
+                    getSupportFragmentManager().findFragmentByTag("FragmentHome");
+            getSupportFragmentManager().beginTransaction()
+                    .attach(fragmentHome)
+                    .detach(mainFragment)
+                    .commit();
+
+        }
+        super.onResume();
+    }
 }
---
+++ app/src/main/java/com/landtanin/studentattendancecheck/fragment/FragmentHome.java
@@ -0,0 +1,69 @@
+package com.landtanin.studentattendancecheck.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.landtanin.studentattendancecheck.R;
+
+
+/**
+ * Created by nuuneoi on 11/16/2014.
+ */
+public class FragmentHome extends Fragment {
+
+    public FragmentHome() {
+        super();
+    }
+
+    public static FragmentHome newInstance() {
+        FragmentHome fragment = new FragmentHome();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init(savedInstanceState);
+
+        if (savedInstanceState != null)
+            onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        initInstances(rootView, savedInstanceState);
+        return rootView;
+    }
+
+    @SuppressWarnings("UnusedParameters")
+    private void init(Bundle savedInstanceState) {
+        // Init Fragment level's variable(s) here
+    }
+
+    @SuppressWarnings("UnusedParameters")
+    private void initInstances(View rootView, Bundle savedInstanceState) {
+        // Init 'View' instance(s) with rootView.findViewById here
+        // Note: State of variable initialized here could not be saved
+        //       in onSavedInstanceState
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save Instance (Fragment level's variables) State here
+    }
+
+    @SuppressWarnings("UnusedParameters")
+    private void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Restore Instance (Fragment level's variables) State here
+    }
+
+}
