27df5ce6354529808476817ffa70895480576b0d
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-04-11 11:02:42 +0100

    problem with Realm

--- app/src/main/java/com/landtanin/studentattendancecheck/manager/HttpManager.java
+++ app/src/main/java/com/landtanin/studentattendancecheck/manager/HttpManager.java
@@ -1,12 +1,24 @@
 package com.landtanin.studentattendancecheck.manager;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
 import com.landtanin.studentattendancecheck.manager.http.ApiService;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -17,11 +29,86 @@ public class HttpManager {
 
     private static HttpManager instance;
 
-    public static HttpManager getInstance() {
-        if (instance == null)
-            instance = new HttpManager();
-        return instance;
+    public static final String BASE_URL = "http://192.168.0.19/api/";
+    private static Retrofit retrofit = null;
+    private static Gson gson;
+
+//    public static HttpManager getInstance() {
+//
+//        if (instance == null)
+//            instance = new HttpManager();
+//        return instance;
+//    }
+
+    // ----------------------------------------------------------------
+
+    public static Retrofit getInstance(){
+
+        if(retrofit==null){
+
+            gson=new GsonBuilder().setPrettyPrinting().create();
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(new LoggingInterceptor())
+                    .connectTimeout(10, TimeUnit.MINUTES)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                    .client(client)
+                    .build();
+
+        }
+
+        return retrofit;
+
     }
+
+    private static class LoggingInterceptor implements Interceptor {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request request = chain.request();
+
+            long t1 = System.nanoTime();
+            System.out.println(
+                    String.format("intercept Sending request %s on %s%n%s", request.url(), chain.connection(),
+                            request.headers()));
+
+            //Logข้อมูลทั้งหมด
+
+            Log.e("intercept: ", String.valueOf(request.url()));
+
+            Response response = chain.proceed(request);
+
+            long t2 = System.nanoTime();
+            System.out.println(
+                    String.format("intercept Received response for %s in %.1fms%n%s", response.request().url(),
+                            (t2 - t1) / 1e6d, response.headers()));
+
+            String bodyString = response.body().string();
+
+            //Logข้อมูลทั้งหมด
+
+            Log.e( "intercept: ", bodyString);
+            try {
+                JSONObject jsonObject = new JSONObject(bodyString);
+
+////                Logข้อมูลJsonทั้งหมด
+                //  Log.e(TAG,gson.toJson(jsonObject));
+
+            } catch (JSONException e) {
+
+                e.printStackTrace();
+            }
+
+            return response.newBuilder()
+                    .body(ResponseBody.create(response.body().contentType(), bodyString))
+                    .build();
+        }
+    }
+
+    // ----------------------------------------------------------------
 
     private Context mContext;
     private ApiService service;
@@ -29,14 +116,14 @@ public class HttpManager {
     private HttpManager() {
         mContext = Contextor.getInstance().getContext();
 
-        Gson gson = new GsonBuilder()
+        Gson gson2 = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd HH:mm:ss")
                 .create();
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.0.19/api/")
+                .baseUrl(BASE_URL)
 //                .baseUrl("https://nuuneoi.com/courses/500px/")
-                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addConverterFactory(GsonConverterFactory.create(gson2))
                 .build();
 
         service = retrofit.create(ApiService.class);
